b4978c62fd584948ec46b8472fb188b9130a7e19
Gopala chachre <chachregopala@gmail.com>
2026-02-01 23:03:50 +0530

    max profit by selling stock

---
+++ LeetCode/MaxProfit_1.java
@@ -0,0 +1,15 @@
+class MaxProfit_1 {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0], maxProfit = 0;
+        for(int i = 1; i < prices.length; i++){
+            if(minPrice > prices[i]){
+                minPrice = prices[i];
+            } else {
+                if(maxProfit < (prices[i] - minPrice)){
+                    maxProfit = prices[i] - minPrice;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
