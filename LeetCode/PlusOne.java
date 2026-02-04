a59ec4646aa0ac2a21859276e280d5ac4128581a
Gopala chachre <chachregopala@gmail.com>
2026-02-04 20:06:45 +0530

    adding +1 in number

---
+++ LeetCode/PlusOne.java
@@ -0,0 +1,16 @@
+class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for(int i = len-1; i >= 0; i--){
+            if(digits[i] == 9){
+                digits[i] = 0;
+            } else {
+                digits[i] = digits[i] + 1;
+                return digits;
+            }
+        }
+        int[] newDigits = new int[len + 1];
+        newDigits[0] = 1;
+        return newDigits;
+    }
+}
