2325251c5b74469536ecb605418ca906c176ef10
Gopala chachre <chachregopala@gmail.com>
2026-02-09 23:26:49 +0530

    Find Max Consecutive Ones

---
+++ LeetCode/FindMaxConsecutiveOnes.java
@@ -0,0 +1,14 @@
+class FindMaxConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0, count = 0;
+        for(int num : nums){
+            if(num == 1){
+                count++;
+                if(count > max) max = count;
+            } else {
+                count = 0;
+            }
+        }
+        return max;
+    }
+}
